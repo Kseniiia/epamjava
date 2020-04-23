ddd6d0016527844ec11d2ef817acc7a99d2ef27b
Kseniiia <xenia-2001@mail.ru>
2020-04-23 19:57:49 +0300

    feat: update controller

--- easytutor/src/main/java/by/bsu/easytutor/controller/StudentController.java
+++ easytutor/src/main/java/by/bsu/easytutor/controller/StudentController.java
@@ -1,14 +1,39 @@
 package by.bsu.easytutor.controller;
 
+import by.bsu.easytutor.dao.DAOFactory;
+import by.bsu.easytutor.entity.Course;
+import by.bsu.easytutor.service.StudentService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+
 @RestController
 public class StudentController {
-    private s
+    private static final String DATABASE = "jdbc:mysql://localhost/easytutor";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "abcd1234";
 
-    @RequestMapping("/students/")
-    public String index() {
-        return "Good luck, Ksusha";
+    @RequestMapping("/")
+    public String index() throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(DATABASE, USERNAME, PASSWORD);
+
+        DAOFactory daoFactory = new DAOFactory(connection);
+
+        StudentService studentService = new StudentService(daoFactory);
+
+        List<Course> courses = studentService.getCourses(1);
+
+        String html = "<table><thead><tr><th>Id</th><th>Name</th></tr><tbody>";
+        for (Course course : courses) {
+            html += "<tr><td>" + course.getId() + "</td><td>" + course.getName() + "</td></tr>";
+        }
+        html += "</tbody></table>";
+
+        return html;
     }
 }
