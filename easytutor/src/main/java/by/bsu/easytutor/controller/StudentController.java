57cc45f76b989d8b2d6608ff3973271c2ff6459b
Kseniiia <xenia-2001@mail.ru>
2020-04-24 13:52:27 +0300

    feat: add html file

--- easytutor/src/main/java/by/bsu/easytutor/controller/StudentController.java
+++ easytutor/src/main/java/by/bsu/easytutor/controller/StudentController.java
@@ -3,33 +3,29 @@ package by.bsu.easytutor.controller;
 import by.bsu.easytutor.entity.Course;
 import by.bsu.easytutor.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
     @RequestMapping(value = "/students/{id}/courses")
-    @ResponseBody
-    public String index(@PathVariable("id") long id) throws Exception{
+    public String index(@PathVariable("id") long id, Model model) throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
 
         List<Course> courses = studentService.getCourses(id);
 
-        String html = "<table><thead><tr><th>Id</th><th>Name</th></tr></tbody>";
-        for (Course course : courses) {
-            html += "<tr><td>" + course.getId() + "</td><td>" + course.getName() + "</td></tr>";
-        }
-        html += "</tbody></table>";
+        model.addAttribute("courses", courses);
 
-        return html;
+        return "studentCourses";
     }
 }
 
